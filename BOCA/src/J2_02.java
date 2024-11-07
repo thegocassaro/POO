f51a1fa09199b13ab7d09374aa771fb8581ab103
Gustavo Cassaro <gustavo.cassaro@edu.ufes.br>
2024-11-06 23:21:38 -0300

    j102

---
+++ BOCA/src/J2_02.java
@@ -0,0 +1,21 @@
+public class J2_02 {
+    
+    public static void main(String[] args) throws Exception {
+
+        int firstHalf = 0;
+        int secondHalf = 0;
+
+        for(int num=1001; num<9999; num++){
+
+            firstHalf = num / 100;
+            secondHalf = num % 100;
+
+            int sum = firstHalf + secondHalf;
+
+            if ( (sum * sum) == num ){
+
+                System.out.println(num);
+            }
+        }
+    }
+}
